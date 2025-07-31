194d2e4161c5c366ed6015017ba33b97eec6eba0
Timeless <Ayomidesamuel365@gmail.com>
2025-07-31 03:49:54 +0100

    feat: Add VendorForm and update imports in purchases/views.py

--- purchases/views.py
+++ purchases/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import RequestForMaterials, Vendor, RequestForQuote, QuotationReceived, PurchaseOrder, Vendor
 from django.contrib import messages
+from .forms import VendorForm
 
 # 📌 View 1: Create Purchase Request
 @login_required
