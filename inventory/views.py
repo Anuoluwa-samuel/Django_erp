228659a1306965b2f406df969bb45779b1b74d1e
Timeless <Ayomidesamuel365@gmail.com>
2025-07-31 01:27:21 +0100

    feat: Implement basic CRUD functionality for products and staff

--- inventory/views.py
+++ inventory/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import TemplateView, View
 from inventory.forms import UserRegisterForm
 from django.contrib.auth import authenticate, login
@@ -32,197 +32,133 @@ class SignUpView(View):
             return redirect ('index')
         return render(request, 'inventory/signup.html', {'form': form}) 
 
-"""
-This module collects helper functions and classes that "span" multiple levels
-of MVC. In other words, these functions/classes introduce controlled coupling
-for convenience's sake.
-"""
 
-from django.http import (
-    Http404,
-    HttpResponse,
-    HttpResponsePermanentRedirect,
-    HttpResponseRedirect,
-)
-from django.template import loader
-from django.urls import NoReverseMatch, reverse
-from django.utils.functional import Promise
+from .models import Product, Category, Order, Staff 
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib import messages
 
+@login_required
+def inventory_list(request):
+    products = Product.objects.all()
+    return render(request, 'inventory/base_inventory.html', {'products': products})
 
-def render(
-    request, template_name, context=None, content_type=None, status=None, using=None
-):
-    """
-    Return an HttpResponse whose content is filled with the result of calling
-    django.template.loader.render_to_string() with the passed arguments.
-    """
-    content = loader.render_to_string(template_name, context, request, using=using)
-    return HttpResponse(content, content_type, status)
-
-
-def redirect(to, *args, permanent=False, preserve_request=False, **kwargs):
-    """
-    Return an HttpResponseRedirect to the appropriate URL for the arguments
-    passed.
-
-    The arguments could be:
-
-        * A model: the model's `get_absolute_url()` function will be called.
-
-        * A view name, possibly with arguments: `urls.reverse()` will be used
-          to reverse-resolve the name.
-
-        * A URL, which will be used as-is for the redirect location.
-
-    Issues a temporary redirect by default. Set permanent=True to issue a
-    permanent redirect. Set preserve_request=True to instruct the user agent
-    to preserve the original HTTP method and body when following the redirect.
-    """
-    redirect_class = (
-        HttpResponsePermanentRedirect if permanent else HttpResponseRedirect
-    )
-    return redirect_class(
-        resolve_url(to, *args, **kwargs),
-        preserve_request=preserve_request,
-    )
-
-
-def _get_queryset(klass):
-    """
-    Return a QuerySet or a Manager.
-    Duck typing in action: any class with a `get()` method (for
-    get_object_or_404) or a `filter()` method (for get_list_or_404) might do
-    the job.
-    """
-    # If it is a model class or anything else with ._default_manager
-    if hasattr(klass, "_default_manager"):
-        return klass._default_manager.all()
-    return klass
-
-
-def get_object_or_404(klass, *args, **kwargs):
-    """
-    Use get() to return an object, or raise an Http404 exception if the object
-    does not exist.
-
-    klass may be a Model, Manager, or QuerySet object. All other passed
-    arguments and keyword arguments are used in the get() query.
-
-    Like with QuerySet.get(), MultipleObjectsReturned is raised if more than
-    one object is found.
-    """
-    queryset = _get_queryset(klass)
-    if not hasattr(queryset, "get"):
-        klass__name = (
-            klass.__name__ if isinstance(klass, type) else klass.__class__.__name__
+@login_required
+def create_product(request):
+    if request.method == 'POST':
+        Product.objects.create(
+            name=request.POST['name'],
+            category=request.POST['category'],
+            quantity=request.POST['quantity']
         )
-        raise ValueError(
-            "First argument to get_object_or_404() must be a Model, Manager, "
-            "or QuerySet, not '%s'." % klass__name
+        messages.success(request, "Product added.")
+        return redirect('product_list')
+    return render(request, 'inventory/create_product.html')
+
+@login_required
+def edit_product(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    categories = Category.objects.all()
+
+    if request.method == 'POST':
+        product.name = request.POST['name']
+        product.category = Category.objects.get(id=request.POST['category'])
+        product.description = request.POST['description']
+        product.quantity = request.POST['quantity']
+        product.unit_price = request.POST['unit_price']
+        product.save()
+        return redirect('inventory_list')
+
+    return render(request, 'inventory/edit_product.html', {'product': product, 'categories': categories})
+
+@login_required
+def delete_product(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+
+    if request.method == 'POST':
+        product.delete()
+        return redirect('inventory_list')
+
+    return render(request, 'inventory/delete_product.html', {'product': product})
+
+@login_required
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'inventory/product_list.html', {'products': products})
+
+def is_admin(user):
+    return user.is_superuser
+
+@user_passes_test(is_admin)
+def staff_list(request):
+    staff = Staff.objects.all()
+    return render(request, 'inventory/staff_list.html', {'staff': staff})
+
+@login_required
+def order_list(request):
+    if request.user.is_superuser:
+        orders = Order.objects.all()
+    else:
+        staff = Staff.objects.get(user=request.user)
+        orders = Order.objects.filter(staff=staff)
+
+    return render(request, 'inventory/order_list.html', {'orders': orders})
+
+@login_required
+def create_order(request):
+    products = Product.objects.all()
+    staff = Staff.objects.all()
+
+    if request.method == 'POST':
+        Order.objects.create(
+            product_id=request.POST['product'],
+            quantity=request.POST['quantity'],
+            staff_id=request.POST['staff'],
+            status='pending'
         )
-    try:
-        return queryset.get(*args, **kwargs)
-    except queryset.model.DoesNotExist:
-        raise Http404(
-            "No %s matches the given query." % queryset.model._meta.object_name
+        messages.success(request, "Order created.")
+        return redirect('order_list')
+
+    return render(request, 'inventory/create_order.html', {'products': products, 'staff': staff})
+
+@login_required
+def create_staff(request):
+    users = User.objects.exclude(id__in=Staff.objects.values_list('user_id', flat=True))
+
+    if request.method == 'POST':
+        Staff.objects.create(
+            user_id=request.POST['user'],
+            role=request.POST['role']
         )
+        messages.success(request, "Staff member added.")
+        return redirect('staff_list')
 
+    return render(request, 'inventory/create_staff.html', {'users': users})
 
-async def aget_object_or_404(klass, *args, **kwargs):
-    """See get_object_or_404()."""
-    queryset = _get_queryset(klass)
-    if not hasattr(queryset, "aget"):
-        klass__name = (
-            klass.__name__ if isinstance(klass, type) else klass.__class__.__name__
-        )
-        raise ValueError(
-            "First argument to aget_object_or_404() must be a Model, Manager, or "
-            f"QuerySet, not '{klass__name}'."
-        )
-    try:
-        return await queryset.aget(*args, **kwargs)
-    except queryset.model.DoesNotExist:
-        raise Http404(f"No {queryset.model._meta.object_name} matches the given query.")
+@login_required
+def edit_staff(request, staff_id):
+    staff = get_object_or_404(Staff, id=staff_id)
 
+    if request.method == 'POST':
+        staff.role = request.POST['role']
+        staff.phone = request.POST['phone']
+        staff.save()
+        messages.success(request, "Staff updated.")
+        return redirect('staff_list')
 
-def get_list_or_404(klass, *args, **kwargs):
-    """
-    Use filter() to return a list of objects, or raise an Http404 exception if
-    the list is empty.
+    return render(request, 'inventory/edit_staff.html', {'staff': staff})
 
-    klass may be a Model, Manager, or QuerySet object. All other passed
-    arguments and keyword arguments are used in the filter() query.
-    """
-    queryset = _get_queryset(klass)
-    if not hasattr(queryset, "filter"):
-        klass__name = (
-            klass.__name__ if isinstance(klass, type) else klass.__class__.__name__
-        )
-        raise ValueError(
-            "First argument to get_list_or_404() must be a Model, Manager, or "
-            "QuerySet, not '%s'." % klass__name
-        )
-    obj_list = list(queryset.filter(*args, **kwargs))
-    if not obj_list:
-        raise Http404(
-            "No %s matches the given query." % queryset.model._meta.object_name
-        )
-    return obj_list
+@login_required
+def delete_staff(request, staff_id):
+    staff = get_object_or_404(Staff, id=staff_id)
 
+    if request.method == 'POST':
+        staff.delete()
+        messages.success(request, "Staff deleted.")
+        return redirect('staff_list')
 
-async def aget_list_or_404(klass, *args, **kwargs):
-    """See get_list_or_404()."""
-    queryset = _get_queryset(klass)
-    if not hasattr(queryset, "filter"):
-        klass__name = (
-            klass.__name__ if isinstance(klass, type) else klass.__class__.__name__
-        )
-        raise ValueError(
-            "First argument to aget_list_or_404() must be a Model, Manager, or "
-            f"QuerySet, not '{klass__name}'."
-        )
-    obj_list = [obj async for obj in queryset.filter(*args, **kwargs)]
-    if not obj_list:
-        raise Http404(f"No {queryset.model._meta.object_name} matches the given query.")
-    return obj_list
+    return render(request, 'inventory/delete_staff.html', {'staff': staff})
 
-
-def resolve_url(to, *args, **kwargs):
-    """
-    Return a URL appropriate for the arguments passed.
-
-    The arguments could be:
-
-        * A model: the model's `get_absolute_url()` function will be called.
-
-        * A view name, possibly with arguments: `urls.reverse()` will be used
-          to reverse-resolve the name.
-
-        * A URL, which will be returned as-is.
-    """
-    # If it's a model, use get_absolute_url()
-    if hasattr(to, "get_absolute_url"):
-        return to.get_absolute_url()
-
-    if isinstance(to, Promise):
-        # Expand the lazy instance, as it can cause issues when it is passed
-        # further to some Python functions like urlparse.
-        to = str(to)
-
-    # Handle relative URLs
-    if isinstance(to, str) and to.startswith(("./", "../")):
-        return to
-
-    # Next try a reverse URL resolution.
-    try:
-        return reverse(to, args=args, kwargs=kwargs)
-    except NoReverseMatch:
-        # If this is a callable, re-raise.
-        if callable(to):
-            raise
-        # If this doesn't "feel" like a URL, re-raise.
-        if "/" not in to and "." not in to:
-            raise
-
-    # Finally, fall back and assume it's a URL
-    return to
+@login_required
+def staff_list(request):
+    staff = Staff.objects.all()
+    return render(request, 'inventory/staff_list.html', {'staff': staff})
