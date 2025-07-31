3c535697cc5bbbf68bc67867fe5b5f7e46ea5255
Timeless <Ayomidesamuel365@gmail.com>
2025-07-31 02:12:21 +0100

    feat: Register models in Django admin using @admin.register

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
-from .models import Staff, Category, Product, Order
+from .models import Staff, Product, Category, Order
 
-admin.site.register(Staff)
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Order)
+@admin.register(Staff)
+class StaffAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role', 'phone')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'quantity', 'unit_price', 'added_on')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('staff', 'product', 'quantity', 'status', 'requested_at')
+    list_filter = ('status',)
+
