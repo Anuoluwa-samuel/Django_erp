51c5694ec9607034f7e8ccb145158659e0f3139f
Timeless <Ayomidesamuel365@gmail.com>
2025-08-10 14:11:51 +0100

    Fix: Import User model from django.contrib.auth.models

--- inventory/views.py
+++ inventory/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from .models import Product, Category, Order, Staff 
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.models import User
 
 
 
