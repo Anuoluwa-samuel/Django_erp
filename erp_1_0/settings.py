0749064925b8b2617d12b7c617918787b521566c
Timeless <Ayomidesamuel365@gmail.com>
2025-08-10 15:14:06 +0100

    feat: Configure email backend and add login/logout URLs

--- erp_1_0/settings.py
+++ erp_1_0/settings.py
@@ -155,3 +155,10 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'ayomidesamuel365@example.com'
+EMAIL_HOST_PASSWORD = 'yourpassword'
