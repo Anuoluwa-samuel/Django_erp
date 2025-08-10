000ce6ecc1e965087939414277f875dbfa10513d
Timeless <Ayomidesamuel365@gmail.com>
2025-08-10 12:42:42 +0100

    Fix: Corrected URL and view function for vendor editing

--- purchases/views.py
+++ purchases/views.py
@@ -120,7 +120,7 @@ def vendor_list(request):
     vendors = Vendor.objects.all()
     return render(request, 'purchases/vendor.html', {'vendors': vendors})
 
-def edit_vendor(request, vendor_id):
+def vendor_edit(request, vendor_id):
     vendor = get_object_or_404(Vendor, id=vendor_id)
     if request.method == 'POST':
         form = VendorForm(request.POST, instance=vendor)
