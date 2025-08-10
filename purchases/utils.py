ed5001439888b1c65934e2231f8200b4546b34c5
Timeless <Ayomidesamuel365@gmail.com>
2025-08-10 15:07:21 +0100

    feat: Add utility function to generate quotation PDF

--- purchases/utils.py
+++ purchases/utils.py
@@ -1,3 +1,10 @@
+# purchases/utils.py
 from django.template.loader import render_to_string
 from weasyprint import HTML
 import tempfile
+
+def generate_quotation_pdf(context):
+    html_string = render_to_string('purchases/quotation_list.html', context)
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as output:
+        HTML(string=html_string).write_pdf(output.name)
+        return output.name
