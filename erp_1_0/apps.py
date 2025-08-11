a2494b37693f26ca6c76005c244f71c6b6ceb8d4
Timeless <Ayomidesamuel365@gmail.com>
2025-08-11 13:48:11 +0100

    Fix: Correct app name and signal import path in CoreConfig

--- erp_1_0/apps.py
+++ erp_1_0/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
+    name = 'erp_1_0'
 
     def ready(self):
-        import .signals  # Import signals so Django registers them
+        import erp_1_0.signals  # Import signals so Django registers them
