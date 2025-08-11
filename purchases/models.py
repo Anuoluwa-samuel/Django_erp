fbbc0b1e5d44d38e7df20df39acfbcc9d4dcf5c7
Timeless <Ayomidesamuel365@gmail.com>
2025-08-11 09:40:57 +0100

    Fix: Allow blank/null vendor names

--- purchases/models.py
+++ purchases/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Vendor(models.Model):
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=150, null=True, blank=True)
     contact_person = models.CharField(max_length=100, blank=True)
     email = models.EmailField(blank=True)
     phone = models.CharField(max_length=15, blank=True)
