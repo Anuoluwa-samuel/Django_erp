20f1d7990fd4c4b2e6045870a955546d93342979
Timeless <Ayomidesamuel365@gmail.com>
2025-08-11 14:28:42 +0100

    Fix: Rename 'send_rfq' URL to 'create_rfq'

--- purchases/urls.py
+++ purchases/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.purchase_request_list, name='purchase_request_list'),
     path('request/', views.create_purchase_request, name='create_purchase_request'),
-    path('rfq/send/', views.send_rfq, name='send_rfq'),
+
+    path('rfq/create/', views.send_rfq, name='create_rfq'),
     path('quotations/', views.quotation_list, name='quotation_list'),
     path('quotation/submit/', views.submit_quotation, name='submit_quotation'),
     path('vendor/new/', views.create_vendor, name='create_vendor'),
