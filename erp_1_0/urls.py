efffb038807bf3ed9423e881b79ede4f48b9299d
Timeless <Ayomidesamuel365@gmail.com>
2025-08-19 10:56:43 +0100

    Refactor: Redirect root URL to dashboard instead of user module.

--- erp_1_0/urls.py
+++ erp_1_0/urls.py
@@ -20,7 +20,7 @@ from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('user.urls')), 
+    path('', include('dashboard.urls')), 
     path('inventory/', include('inventory.urls')),
     path('purchases/', include('purchases.urls')),
     path('', lambda request: redirect('login'), name='home'),
