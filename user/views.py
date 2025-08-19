39beae5d77c59c27b545ac371c0d5bb48ef897e0
Timeless <Ayomidesamuel365@gmail.com>
2025-08-19 10:52:13 +0100

    feat: Implement basic login view

--- user/views.py
+++ user/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            return render(request, 'login.html', {'error': 'Invalid credentials'})
+
+    return render(request, 'login.html')
