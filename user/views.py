03a81caa7ce8e05090818ac0698c335c54e93866
Timeless <Ayomidesamuel365@gmail.com>
2025-08-19 10:53:43 +0100

    feat: Add custom user creation form with email field

--- user/views.py
+++ user/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.shortcuts import render, redirect
-from inventory.models import Staff
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from .forms import UserCreationForm
