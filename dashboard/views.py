6ff21b8b1d9a2d9155ee8bc7cf171f4dcc390a2b
Timeless <Ayomidesamuel365@gmail.com>
2025-08-19 09:21:37 +0100

    Fix: Remove trailing whitespace in dashboard/views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from .forms import UserCreationForm
 from django.contrib import messages
-
+    
 
 
 def login_view(request):
