8b6c3234ba8c28397f177f53c18e65e5c9d5dcd2
Timeless <Ayomidesamuel365@gmail.com>
2025-08-21 12:08:12 +0100

    Fix: Use api_view decorator and remove APIView class

--- purchases/views.py
+++ purchases/views.py
@@ -2,17 +2,11 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required, user_passes_test
 from .models import RequestForMaterials, Vendor, RequestForQuote, QuotationReceived, PurchaseOrder
 from django.contrib import messages
-from rest_framework.views import APIView
+from rest_framework.views import api_view, permission_classes
 from rest_framework.response import Response
 from accounts.permissions import IsAdmin, IsSupervisor, IsStaff, IsAdminOrSupervisor, IsStaffOrSupervisor
 
 
-
-class PurchaseOrderView(APIView):
-    permission_classes = [IsAdmin | IsSupervisor]  # allows either Admin or Supervisor
-
-    def get(self, request):
-        return Response({"message": "Only Admins and Supervisors can access this."})
 
 
 # Check functions
