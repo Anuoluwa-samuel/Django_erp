8b51bbf0d95c91a6bf68f9a9db2f08b4da94f4aa
Timeless <Ayomidesamuel365@gmail.com>
2025-08-23 22:45:37 +0100

    Updated Changes

--- purchases/views.py
+++ purchases/views.py
@@ -167,6 +167,7 @@ def edit_vendor(request, pk):
     return render(request, 'purchases/edit_vendor.html', {'vendor': vendor})
 
 @login_required
+@group_required(["Admin"])
 def delete_vendor(request, pk):
     vendor = get_object_or_404(Vendor, pk=pk)
 
