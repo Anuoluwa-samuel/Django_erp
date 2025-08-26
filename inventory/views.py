df1e3eac345fe868ba01fc741056bea625e9cd18
Ayomide <ayomidesamuel365@gmail.com>
2025-08-26 10:43:58 +0100

    Fix: Remove unused imports in inventory views.py

--- inventory/views.py
+++ inventory/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import TemplateView, View
-from inventory.forms import UserRegisterForm
-from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from .models import Product, Category, Order, Staff 
 from django.contrib.auth.decorators import login_required, user_passes_test
