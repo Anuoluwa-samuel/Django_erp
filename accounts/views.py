201f08ed9a8c1adcee0126725ee30c147c3cf228
Ayomide <ayomidesamuel365@gmail.com>
2025-08-26 10:07:12 +0100

    Refactor: Remove unused dashboard view functions.

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,6 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import render
 
 
-# --- Check functions ---
 def is_admin(user):
     return user.is_superuser or user.groups.filter(name="Admin").exists()
 
@@ -13,18 +12,3 @@ def is_purchases(user):
     return user.groups.filter(name__in=["Purchases Supervisor", "Purchases Staff"]).exists()
 
 
-# --- Views ---
-@login_required
-@user_passes_test(is_inventory)
-def inventory_dashboard(request):
-    return render(request, "inventory/dashboard.html")
-
-@login_required
-@user_passes_test(is_purchases)
-def purchases_dashboard(request):
-    return render(request, "purchases/dashboard.html")
-
-@login_required
-@user_passes_test(is_admin)
-def admin_dashboard(request):
-    return render(request, "admin/dashboard.html")
