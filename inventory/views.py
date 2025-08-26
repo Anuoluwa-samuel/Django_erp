f4c415bb9a12a820fd62a6835dfb8cbf221e98f7
Ayomide <ayomidesamuel365@gmail.com>
2025-08-26 10:34:12 +0100

    Fix: Allow staff users to view all orders

--- inventory/views.py
+++ inventory/views.py
@@ -85,7 +85,7 @@ def product_list(request):
 @login_required
 @user_passes_test(is_inventory)
 def order_list(request):
-    if request.user.is_superuser:
+    if request.user.is_staff:
         orders = Order.objects.all()
     else:
         staff = Staff.objects.get(user=request.user)
