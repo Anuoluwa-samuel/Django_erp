a48d0c95068e54359a904bab55734a60e96a74da
Timeless <Ayomidesamuel365@gmail.com>
2025-09-02 20:38:16 +0100

    feat: Remove Staff model from dashboard app

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,9 +1,1 @@
 from django.db import models
-
-class Staff(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=100)
-    phone = models.CharField(max_length=15)
-
-    def __str__(self):
-        return self.user.username
