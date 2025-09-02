db89923a610d37356c43326e3cdb9d09fae59335
Timeless <Ayomidesamuel365@gmail.com>
2025-09-02 18:27:10 +0100

    Fix: Correct MySQL port to 3306 in settings.py

--- erp_1_0/settings.py
+++ erp_1_0/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
         'USER': 'Ayomide',
         'PASSWORD': 'password',  
         'HOST': 'db',
-        'PORT': '3307',
+        'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         }
