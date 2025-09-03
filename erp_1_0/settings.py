a41b309fc70a44f8c86470b945dfcc54864147d3
Timeless <Ayomidesamuel365@gmail.com>
2025-09-03 11:03:02 +0100

    Fix: Corrected typo in database host setting.

--- erp_1_0/settings.py
+++ erp_1_0/settings.py
@@ -132,8 +132,8 @@ DATABASES = {
         'NAME': "postgres",
         'USER':"postgres",
         'PASSWORD': "postgres",
-        'HOST': "127.0.0.1",
-        'PORT':5432,
+        'HOST': "localhots"
+        'PORT': 5432,
     }
 }
 
