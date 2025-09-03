f50fdc11ba9742789469e760d25428c608b10656
Timeless <Ayomidesamuel365@gmail.com>
2025-09-03 09:10:00 +0100

    Fix: Import tenant_admin_site from tenant_manager.admin

--- erp_1_0/urls_public.py
+++ erp_1_0/urls_public.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
-from tenant_manager import
+from tenant_manager.admin import tenant_admin_site
 
 urlpatterns = [
     path('admin/', admin.site.urls),
