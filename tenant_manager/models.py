cb67b4ecde0fa093fded5d5c3886584416574ce1
Timeless <Ayomidesamuel365@gmail.com>
2025-09-03 08:42:00 +0100

    Fix: Inherit Domain model from DomainMixin

--- tenant_manager/models.py
+++ tenant_manager/models.py
@@ -1,8 +1,10 @@
 from django.db import models
-from django_tenants.models import TenantMixin
+from django_tenants.models import TenantMixin, DomainMixin
 
 class Tenant(TenantMixin):
     name = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
 
-class Domain
+class Domain(DomainMixin):
+    pass
+
