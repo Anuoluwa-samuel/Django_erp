beb0b07d7724231d836a12f9718fc085828835b7
Timeless <Ayomidesamuel365@gmail.com>
2025-09-03 08:18:00 +0100

    Fix: Correct typo in INSTALLED_APPS for djano_tenants

--- erp_1_0/settings.py
+++ erp_1_0/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "djano"
+    "djano_tenants"
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
