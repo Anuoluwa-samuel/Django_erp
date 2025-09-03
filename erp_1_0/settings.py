f5739f1846d1fc6bb45b708007f7a5cbcc965611
Timeless <Ayomidesamuel365@gmail.com>
2025-09-03 10:28:33 +0100

    Refactor: Load SECRET_KEY and DEBUG from environment variables

--- erp_1_0/settings.py
+++ erp_1_0/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-r)=&b!@+tpno6-i5n*zjgy1y^%3)dk5*!++uc^%-v84!3n&$rr"
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
