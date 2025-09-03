f4eea0d917883590023786690e27c34bd146d45d
Timeless <Ayomidesamuel365@gmail.com>
2025-09-03 08:41:00 +0100

    feat(tenant): Add created timestamp and limit name field length

--- tenant_manager/models.py
+++ tenant_manager/models.py
@@ -2,4 +2,5 @@ from django.db import models
 from django_tenants.models import TenantMixin
 
 class Tenant(TenantMixin):
-    name = models.CharField
+    name = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
