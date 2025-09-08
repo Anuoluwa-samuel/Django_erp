f77b802ef1718815128a9f7ae511dfe115fb3ed0
Timeless <Ayomidesamuel365@gmail.com>
2025-09-08 10:54:42 +0100

    feat: Configure media file serving

--- erp_1_0/settings.py
+++ erp_1_0/settings.py
@@ -203,7 +203,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
-
+MEDIA_URL = 'media/'
+MEDIAL_ROOT = 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
