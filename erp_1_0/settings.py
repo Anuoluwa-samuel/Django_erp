0ea37bb71c42ebf521e3c784a2447e77cab36794
Timeless <Ayomidesamuel365@gmail.com>
2025-09-08 12:03:38 +0100

    Fix: Add database engine check for debugging

--- erp_1_0/settings.py
+++ erp_1_0/settings.py
@@ -226,3 +226,6 @@ LOGOUT_REDIRECT_URL = '/login/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 ACCOUNT_LOGIN_METHODS = {'email', 'username'}
 ACCOUNT_SIGNUP_FIELDS = ['email*', 'username*', 'password1*', 'password2*']
+
+from django.db import connections
+print(">>> DB ENGINE:", connections['default'].__class__.__module__)
