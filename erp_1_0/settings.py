5dacaa3bef2278edbcafc18c49a57e5d3a4cc293
Timeless <Ayomidesamuel365@gmail.com>
2025-09-08 10:53:42 +0100

    Fix: Correct typo in DEBUG middleware settings

--- erp_1_0/settings.py
+++ erp_1_0/settings.py
@@ -112,8 +112,9 @@ MIDDLEWARE = [
 ]
 
 if DEBUG:
-    MIDDLEWARE +=['django_browser_reload.middleware.BrowserReloadMiddleware'
-    '']
+    MIDDLEWARE +=['django_browser_reload.middleware.BrowserReloadMiddleware']
+
+
 
 ROOT_URLCONF = "erp_1_0.urls"
 PUBLIC_SCHEMA_URLCONF = "erp_1_0.urls_public"
