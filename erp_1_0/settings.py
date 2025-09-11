ef07644bed35e683662ac8ea38ff92f2a49219ad
Timeless <Ayomidesamuel365@gmail.com>
2025-09-11 09:55:51 +0100

    Remove .env, use env vars in settings.py

--- erp_1_0/settings.py
+++ erp_1_0/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Environment (development / production)
 ENVIRONMENT = env("ENVIRONMENT", default="development")
-ENVIRONMENT = "production"
+# ENVIRONMENT = "production"
 
 # Security
 SECRET_KEY = env("SECRET_KEY", default="secret_key")
