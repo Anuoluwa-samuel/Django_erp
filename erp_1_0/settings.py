345dbc7f2936eee64f3b0b33d6d9d47fc7f1ddbf
Timeless <Ayomidesamuel365@gmail.com>
2025-09-11 10:14:22 +0100

    Fix: Correctly quote media URL and root in settings.py

--- erp_1_0/settings.py
+++ erp_1_0/settings.py
@@ -187,8 +187,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default PK
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
