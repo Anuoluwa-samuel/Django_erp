ceff971a9ca9c7cb4b068841e6b86e76805718ec
Ogunyemi Ayomide Samuel <ayomidesamuel365@gmail.com>
2025-09-23 12:08:45 +0100

    Fix: Define STATIC_ROOT and STATICFILES_DIRS using os.path.join

--- erp_1_0/settings.py
+++ erp_1_0/settings.py
@@ -173,8 +173,9 @@ USE_TZ = True
 
 # Static & Media
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
