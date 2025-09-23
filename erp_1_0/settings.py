a9e3ed995d1c149f525b552e456a349ac718ad27
Ogunyemi Ayomide Samuel <ayomidesamuel365@gmail.com>
2025-09-23 12:56:02 +0100

    Fix: Reorder ALLOWED_HOSTS for local development priority

--- erp_1_0/settings.py
+++ erp_1_0/settings.py
@@ -37,7 +37,7 @@ if ENVIRONMENT == 'development':
 else:
     DEBUG = False
 
-ALLOWED_HOSTS = ["djangoerp-production.up.railway.app", "localhost", "127.0.0.1", ]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "djangoerp-production.up.railway.app"]
 
 CSRF_TRUSTED_ORIGINS = ["https://djangoerp-production.up.railway.app"]
 
