0e1437a630b13dc0b411ca3f8a71dd9dfb453bca
Ogunyemi Ayomide Samuel <ayomidesamuel365@gmail.com>
2025-09-30 10:31:58 +0100

    feat: Implement user registration form with email and phone number fields

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,11 +1,23 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
 
-@receiver(post_save, sender=User)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-    else:
-        instance.profile.save()
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    phone = forms.CharField(max_length=20, required=False)
+
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "email", "username", "phone", "password1", "password2"]
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+            # Save profile extra field
+            Profile.objects.update_or_create(user=user, defaults={
+                "phone": self.cleaned_data.get("phone"),
+            })
+        return user
