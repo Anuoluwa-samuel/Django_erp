3440e44fa4b9756f7c4b59ab60c05ce5d78f0df9
Ogunyemi Ayomide Samuel <ayomidesamuel365@gmail.com>
2025-09-30 09:59:57 +0100

    feat: Remove SiteSetting model and related dependencies

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,9 +1,1 @@
 from django.db import models
-from colorfield.fields import ColorField
-
-class SiteSetting(models.Model):
-    name = models.CharField(max_length=100)
-    color = ColorField(default="000000")
-
-    def __str__(self):
-        return self.name
