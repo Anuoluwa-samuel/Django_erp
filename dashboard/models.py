5265ec2e6e6bca26b5ae74018901800ba2d3a6b5
Ogunyemi Ayomide Samuel <ayomidesamuel365@gmail.com>
2025-09-30 10:28:28 +0100

    Refactor: Replace CustomUser model with Profile model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class CustomUser(User):
-    phone = models.CharField(max_length=20, blank=True, null=True)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
 
     def __str__(self):
-        return self.username
+        return self.user.username
