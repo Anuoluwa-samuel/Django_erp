923c89324ef9ad376ee4d0ad0c0fd6901686dfb5
Ogunyemi Ayomide Samuel <ayomidesamuel365@gmail.com>
2025-09-30 10:27:57 +0100

    Fix: Inherit from User instead of AbstractUser in CustomUser model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class CustomUser(AbstractUser):
+class CustomUser(User):
     phone = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
