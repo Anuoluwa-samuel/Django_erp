00b5360193b8d513cedd0d355a3f879ec1a9efc3
Ogunyemi Ayomide Samuel <ayomidesamuel365@gmail.com>
2025-09-30 10:04:57 +0100

    feat: Set custom user model in settings.py

--- erp_1_0/settings.py
+++ erp_1_0/settings.py
@@ -154,6 +154,9 @@ TENANT_MODEL = "tenant_manager.Tenant"
 TENANT_DOMAIN_MODEL = "tenant_manager.Domain"
 SHOW_PUBLIC_IF_NO_TENANT_FOUND = True
 
+AUTH_USER_MODEL = "yourapp.CustomUser"
+
+
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
