4085b46441a7ae9798a47ccc18b04ba891930725
Ogunyemi Ayomide Samuel <ayomidesamuel365@gmail.com>
2025-09-30 10:22:58 +0100

    Fix: Extend AbstractUser instead of User for CustomUser model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
-class CustomUser(User):
+class CustomUser(AbstractUser):
     phone = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
