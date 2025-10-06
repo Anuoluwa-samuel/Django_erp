68f23d5dc222298155f662fbfde16a0fa71558dd
Ogunyemi Ayomide Samuel <ayomidesamuel365@gmail.com>
2025-10-06 11:40:24 +0100

    Refactor: Remove unused imports and routes from urls.py

--- erp_1_0/urls.py
+++ erp_1_0/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from dashboard import views as user_view
-from django.contrib.auth import views as auth_views
 from django.shortcuts import redirect
 from django.conf import settings
 
@@ -11,11 +9,6 @@ urlpatterns = [
     path('inventory/', include('inventory.urls')),
     path('purchases/', include('purchases.urls')),
     path('', lambda request: redirect('login'), name='home'),
-
-    path('register/', user_view.register , name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('login/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
 ]
 
 if getattr(settings, "ENVIRONMENT", "development") == "development":
