79df601d692eb0893851b4a06c21f3966905be84
Ogunyemi Ayomide Samuel <ayomidesamuel365@gmail.com>
2025-10-06 12:27:56 +0100

    Fix: Correct typo in EMAIL_HOST_USER

--- erp_1_0/settings.py
+++ erp_1_0/settings.py
@@ -191,7 +191,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'slowlybeginning@gmai'
+EMAIL_HOST_USER = 'slowlybeginning@gmail.com'
 EMAIL_HOST_PASSWORD = ''
 
 ACCOUNT_LOGIN_METHODS = {"email", "username"}
