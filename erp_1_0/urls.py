f3d694d468cf65c3a6e5ebbc291a6fe3621f8c91
Ogunyemi Ayomide Samuel <ayomidesamuel365@gmail.com>
2025-10-06 11:39:24 +0100

    Fix: Add login URL and correct logout URL.

--- erp_1_0/urls.py
+++ erp_1_0/urls.py
@@ -12,7 +12,8 @@ urlpatterns = [
     path('purchases/', include('purchases.urls')),
     path('', lambda request: redirect('login'), name='home'),
 
-    path('register/', user_view.register , name='register'),,
+    path('register/', user_view.register , name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('login/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
 ]
