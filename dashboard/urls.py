eaa5fff82cb4fc73cd21887486daa1a8ac7bf584
Ogunyemi Ayomide Samuel <ayomidesamuel365@gmail.com>
2025-10-06 12:11:54 +0100

    Fix: Corrected URL for password reset confirmation.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset_confirm/', auth_views.PasswordResetonfirmVcew.as_view(), name='password_reset_confirm'),
 ]
 
