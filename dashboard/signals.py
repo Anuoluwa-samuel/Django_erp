c7e4d9630d53caa5bf835c3fff054918f81c34a1
Ogunyemi Ayomide Samuel <ayomidesamuel365@gmail.com>
2025-10-06 23:06:07 +0100

    feat: Send registration notifications to admin and user

--- dashboard/signals.py
+++ dashboard/signals.py
@@ -17,3 +17,52 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     else:
         Profile.objects.get_or_create(user=instance)
         instance.profile.save()
+
+
+User = get_user_model()
+
+@receiver(post_save, sender=User)
+def send_registration_notifications(sender, instance, created, **kwargs):
+    if created:
+        # --- Notify Admin ---
+        subject_admin = "New User Registration Alert"
+        message_admin = render_to_string("emails/admin_new_user.html", {
+            "user": instance,
+        })
+        send_mail(
+            subject_admin,
+            strip_tags(message_admin),
+            settings.DEFAULT_FROM_EMAIL,
+            [settings.DEFAULT_FROM_EMAIL],
+            html_message=message_admin,
+        )
+
+        # --- Notify User ---
+        subject_user = "Account Created Successfully"
+        message_user = render_to_string("emails/user_registration_success.html", {
+            "user": instance,
+        })
+        send_mail(
+            subject_user,
+            strip_tags(message_user),
+            settings.DEFAULT_FROM_EMAIL,
+            [instance.email],
+            html_message=message_user,
+        )
+
+        # --- Optional Real-time Notification (django-notifications) ---
+        # Notify Admin
+        admin_user = User.objects.filter(is_superuser=True).first()
+        if admin_user:
+            notify.send(
+                instance,
+                recipient=admin_user,
+                verb=f"New user '{instance.username}' just registered and awaits activation."
+            )
+
+        # Notify the User
+        notify.send(
+            admin_user,
+            recipient=instance,
+            verb="Your account has been created successfully. You will be notified once it is activated."
+        )
