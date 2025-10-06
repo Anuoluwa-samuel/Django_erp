bbc3bd5385c1a0db0316af225ea4011885b5d492
Ogunyemi Ayomide Samuel <ayomidesamuel365@gmail.com>
2025-10-06 12:29:25 +0100

    Fix: Set correct email password in settings

--- erp_1_0/settings.py
+++ erp_1_0/settings.py
@@ -192,7 +192,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'slowlybeginning@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = 'Ay_mide17.'
 
 ACCOUNT_LOGIN_METHODS = {"email", "username"}
 ACCOUNT_SIGNUP_FIELDS = ["email*", "username*", "password1*", "password2*"]
