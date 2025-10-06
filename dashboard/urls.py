62ef8f0c4b6b963771617f0671cb10fa88bc237b
Ogunyemi Ayomide Samuel <ayomidesamuel365@gmail.com>
2025-10-06 12:10:54 +0100

    Fix: Correct password reset URL pattern

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('register/', register, name='register'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset')
-    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset')
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
 ]
 
