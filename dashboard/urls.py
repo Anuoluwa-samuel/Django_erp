618c1c4af1ab5a34226eb7458c98dbad763dd79b
Ogunyemi Ayomide Samuel <ayomidesamuel365@gmail.com>
2025-10-06 11:33:54 +0100

    feat: Comment out dashboard URLs

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,16 +1,16 @@
-from django.urls import path
-from .views import dashboard, login, logout, register
-from django.contrib.auth import views as auth_views
+# from django.urls import path
+# from .views import dashboard, login, logout, register
+# from django.contrib.auth import views as auth_views
 
-urlpatterns = [
-    path('', dashboard, name='dashboard'),
-    path('login/', login, name='login'),
-    path('logout/', logout, name='logout'),
-    path('register/', register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('login/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+# urlpatterns = [
+#     path('', dashboard, name='dashboard'),
+#     path('login/', login, name='login'),
+#     path('logout/', logout, name='logout'),
+#     path('register/', register, name='register'),
+#     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+#     path('login/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+#     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
 
 
-]
+# ]
 
