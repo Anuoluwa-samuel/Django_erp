6be9a617f18e7635cb55cce052b7d999a2862817
Ogunyemi Ayomide Samuel <ayomidesamuel365@gmail.com>
2025-10-06 11:15:23 +0100

    Fix: Remove duplicate login URL pattern

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('register/', register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    
+    path('login/', auth_views.LogoutView.as_view(template_name='logout.html'), name='login'),
+
 ]
 
