a2b29cca819c1a95e22ca0ee06b36dd34e68cbee
Ogunyemi Ayomide Samuel <ayomidesamuel365@gmail.com>
2025-10-06 23:02:07 +0100

    Fix: Implement new user notification via email

--- dashboard/signals.py
+++ dashboard/signals.py
@@ -2,6 +2,13 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Profile
+from django.conf import settings
+from django.dispatch import receiver
+from django.core.mail import send_mail
+from django.contrib.auth import get_user_model
+from django.utils.html import strip_tags
+from django.template.loader import render_to_string
+from notifications.signals import notify 
 
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
