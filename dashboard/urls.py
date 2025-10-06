154c291a4ede6ba7122be79722c243c04c101439
Ogunyemi Ayomide Samuel <ayomidesamuel365@gmail.com>
2025-10-06 11:32:54 +0100

    Refactor: Rename dashboard view functions for clarity

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
-from .views import dashboard_view, login_view, logout_view, register
+from .views import dashboard, login, logout, register
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', dashboard_view, name='dashboard'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
+    path('', dashboard, name='dashboard'),
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
     path('register/', register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('login/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    
+
 
 ]
 
