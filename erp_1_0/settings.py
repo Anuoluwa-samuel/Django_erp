85eb50ff838811a8db916307b22d0f73d8f37d24
Ogunyemi Ayomide Samuel <ayomidesamuel365@gmail.com>
2025-10-06 12:18:55 +0100

    Fix: Enable TLS for email backend

--- erp_1_0/settings.py
+++ erp_1_0/settings.py
@@ -190,7 +190,7 @@ LOGOUT_REDIRECT_URL = "/login/"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
-EMAIL_USE_TLS = 
+EMAIL_USE_TLS = True
 
 ACCOUNT_LOGIN_METHODS = {"email", "username"}
 ACCOUNT_SIGNUP_FIELDS = ["email*", "username*", "password1*", "password2*"]
