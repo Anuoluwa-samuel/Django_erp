ccc4da3dcea1368edbaf8342f32a26e74ae10479
Ogunyemi Ayomide Samuel <ayomidesamuel365@gmail.com>
2025-10-06 23:01:37 +0100

    feat: Set DEFAULT_FROM_EMAIL to EMAIL_HOST_USER

--- erp_1_0/settings.py
+++ erp_1_0/settings.py
@@ -193,6 +193,7 @@ EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'slowlybeginning@gmail.com'
 EMAIL_HOST_PASSWORD = 'gyzr lxmd ynmj ciow'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 ACCOUNT_LOGIN_METHODS = {"email", "username"}
 ACCOUNT_SIGNUP_FIELDS = ["email*", "username*", "password1*", "password2*"]
