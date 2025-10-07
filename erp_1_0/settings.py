467d5653ad43150d61351641301b02f2de17b789
Ogunyemi Ayomide Samuel <ayomidesamuel365@gmail.com>
2025-10-07 14:09:29 +0100

    Fix: Use console email backend for development

--- erp_1_0/settings.py
+++ erp_1_0/settings.py
@@ -189,7 +189,7 @@ LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/login/"
 
 # Email backend (console for now)
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
