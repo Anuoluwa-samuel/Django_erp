1e81401a91bd3396c02fdde0b8eddbf86e233e92
Ogunyemi Ayomide Samuel <ayomidesamuel365@gmail.com>
2025-10-08 09:38:16 +0100

    feat: Disable direct Gmail SMTP in production

--- erp_1_0/settings.py
+++ erp_1_0/settings.py
@@ -191,8 +191,8 @@ LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/login/"
 
 EMAIL_BACKEND = "anymail.backends.sendgrid.EmailBackend"
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'Ayomidesamuel365@gmail.com'
 EMAIL_HOST_PASSWORD = 'BrBj yggA HuHS'
