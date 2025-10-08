27e4ebe97374f5ddd387476585daee6b4731cee3
Ogunyemi Ayomide Samuel <ayomidesamuel365@gmail.com>
2025-10-08 09:37:46 +0100

    Fix: Set default from email address

--- erp_1_0/settings.py
+++ erp_1_0/settings.py
@@ -196,7 +196,7 @@ EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'Ayomidesamuel365@gmail.com'
 EMAIL_HOST_PASSWORD = 'BrBj yggA HuHS'
-DEFAULT_FROM_EMAIL = ''
+DEFAULT_FROM_EMAIL = 'ayomidesamuel365@gmail.com'
 
 
 
