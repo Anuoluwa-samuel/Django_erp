6e01d8e660b7eef5a3d8ad020ea263d45493bda8
Ogunyemi Ayomide Samuel <ayomidesamuel365@gmail.com>
2025-10-09 09:31:16 +0100

    feat: Add URLs for terms of service, privacy policy, and cookies policy pages.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -11,5 +11,8 @@ urlpatterns = [
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
     path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
     path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
+    path('terms-of-service/', terms_of_service, name='terms_of_service'),
+    path('privacy-policy/', privacy_policy, name='privacy_policy'),
+    path('cookies-policy/', cookies_policy, name='cookies_policy'),
 ]
 
